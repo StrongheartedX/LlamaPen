84734d8d5f7597f4f4f279d1c1883176998a6e58
ImDarkTom <darktomg21@gmail.com>
2026-06-03 03:24:26 +0100

    feat(ui): optional tooltip & types for form fields

--- src/components/UI/FormField.vue
+++ src/components/UI/FormField.vue
@@ -1,26 +1,50 @@
 <script setup lang="ts">
 import { Label } from 'reka-ui';
+import { BiHelpCircle, BiHide, BiShow } from 'vue-icons-plus/bi';
 
-defineProps<{
+withDefaults(defineProps<{
     label: string;
+    type?: HTMLInputElement['type'];
+    tooltip?: string;
     placeholder?: string;
     required?: HTMLInputElement['required'];
     list?: string;
-}>();
+}>(), {
+    type: 'text',
+});
 
 const value = defineModel();
 const id = useId();
+
+const showingPassword = ref(false);
 </script>
 
 <template>
     <div>
-        <Label :for="id" >{{ label }}</Label>
-        <UIInput 
-            type="text" 
-            :placeholder
-            v-model="value"
-            :required
-            :list
-            :id />
+        <div class="inline-flex items-center gap-1">
+            <Label :for="id">{{ label }}</Label>
+            <Tooltip
+                v-if="tooltip"
+                :text="tooltip"
+                size="small">
+                <BiHelpCircle class="size-4 text-base-300 cursor-help" />
+            </Tooltip>
+        </div>
+        <div class="flex flex-row gap-2">
+            <UIInput 
+                :type="showingPassword ? 'text' : type"
+                :placeholder
+                v-model="value"
+                :required
+                :list
+                :id />
+            <button
+                v-if="type === 'password'"
+                class="p-4 hover:bg-base-600 rounded-md"
+                @click="showingPassword = !showingPassword">
+                <BiShow v-if="!showingPassword" />
+                <BiHide v-else />
+            </button>
+        </div>
     </div>
 </template>
