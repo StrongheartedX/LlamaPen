1af77865c1254906dc43cd85750c7827bed46b12
ImDarkTom <darktomg21@gmail.com>
2026-06-03 03:23:30 +0100

    feat(providers): allow updating providers

--- src/stores/useCustomProvidersStore.ts
+++ src/stores/useCustomProvidersStore.ts
@@ -16,12 +16,19 @@ export const useCustomProvidersStore = defineStore('customProvidersStore', () =>
         return key;
     }
 
+    function update(key: string, updatedFields: Partial<Omit<CustomProvider, 'key'>>) {
+        const provider = providers.value.find((p) => p.key === key);
+        if (provider) {
+            Object.assign(provider, updatedFields);
+        }
+    }
+
     function remove(key: string) {
         const foundIndex = providers.value.findIndex((p) => p.key === key);
         if (foundIndex !== -1) providers.value.splice(foundIndex, 1);
     }
 
-    return { providers, add, remove }
+    return { providers, add, update, remove }
 }, {
     persist: true,
 });
